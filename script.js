9673512dd066ee9ceff59eb7672ac671b31eea4d
João Matos <joaoapcm@gmail.com>
2021-03-01 19:12:13 +0000

    header + navbar

---
+++ script.js
@@ -0,0 +1,7 @@
+function navFunc() {
+    let navIcon = document.querySelector(".toggle");
+    let nav = document.querySelector('nav');
+
+    navIcon.classList.toggle('active');
+    nav.classList.toggle('active');
+}
