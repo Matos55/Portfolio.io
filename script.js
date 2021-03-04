501fd10a225cc2feb6f7b1d8eb5ee689ca4a6b8f
João Matos <joaoapcm@gmail.com>
2021-03-04 19:08:34 +0000

    several fixings

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 /* Section 1 */
 /*********************/
 
-
+// toggle nav mobile
 function navFunc() {
     let navIcon = document.querySelector(".toggle");
     let nav = document.querySelector('nav');
@@ -11,3 +11,52 @@ function navFunc() {
     navIcon.classList.toggle('active');
     nav.classList.toggle('active');
 }
+
+
+
+
+/*********************/
+/* Section 4 */
+/*********************/
+
+// Form Validation
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    name: /^[a-z\d]{5,12}$/i,
+    subject: /^[a-z\d]{5,30}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,10})(\.[a-z]{2,8})?$/ 
+
+    // name: 5 to 12 characters. Can be 'a to z' (([a-z])) and any number from '0 to 9' (([\d]))
+
+    // subject: 5 to 20 characters. Can be 'a to z' and any number from '0 to 9'
+
+    // Email Notes:
+        // (yourname) @ (domain) . (extension) (.again)             
+        // (yourname) = any letters, numbers, dots and/or hyphens
+        // (domain) = any letters, numbers and/or hyphens
+        // (extension) = any letters
+        // (.again) = a dot (.) then any letters
+        // ? ====> o ou mais no elemento anterior, neste caso, '(.again)'
+}
+
+function validate(field, regex){
+
+    console.log(regex.test(field.value));
+
+    if(regex.test(field.value)) {
+       field.classList.remove('invalid');
+       field.classList.add('valid');
+   } else {
+       field.classList.add('invalid');
+   }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        
+        //console.log(e.target.attributes.name.value);
+        validate(e.target, patterns[e.target.attributes.name.value]);   // estamos a invocar a função "validate" sendo que o 2º argumento é a constante "patterns[atributo]"
+    });
+
+})
